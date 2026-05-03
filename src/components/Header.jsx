3be597f5aaa991b8cc741708f31b9d3412f5e303
Sharon Moegi <sharamoegi@gmail.com>
2026-05-03 19:43:55 +0300

    Complete tests

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header({name}){
+    return(
+        <div>
+            <header>
+           <h1>{name}</h1> 
+           </header>
+        </div>
+    )
+}
+export default Header;
